94e76e8e6b3765e3dcc139df122fd329ec5c57a3
Rizky Fajri <mansamussolini@gmail.com>
2024-06-14 16:05:49 +0700

    interface added

---
+++ TubesKelompok/src/main/java/com/mycompany/tubeskelompok/makanan.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.tubeskelompok;
+
+/**
+ *
+ * @author Fajri
+ */
+public interface makanan {
+    void makan(String pesanan);
+}
